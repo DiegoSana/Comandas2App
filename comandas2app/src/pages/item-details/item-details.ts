1f5f2dba6c8777be0733d2d7c08006b6786bf041
Diego Sanabria <diegohsanabria@gmail.com>
2018-04-20 17:07:34 -0300

    spinner loading

--- comandas2app/src/pages/item-details/item-details.ts
+++ comandas2app/src/pages/item-details/item-details.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, LoadingController} from 'ionic-angular';
 import {RestapiProvider} from "../../providers/restapi/restapi";
 import {Producto} from "../../models/producto";
 
@@ -12,19 +12,36 @@ export class ItemDetailsPage {
 
     selectedItem: any;
     producto = {} as Producto;
+    public loading: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestapiProvider) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestapiProvider, public loadingCtrl: LoadingController) {
+        this.presentLoadingDefault();
         this.selectedItem = navParams.get('item');
     }
 
     ngOnInit() {
         this.getProducto();
-        //console.log(this.producto);
+    }
+
+    public presentLoadingDefault() {
+        this.loading = this.loadingCtrl.create({
+            //spinner: 'dots',
+            //spinner: 'bubbles',
+            //spinner: 'circles',
+            spinner: 'crescent',
+        });
+
+        this.loading.present();
+
+        setTimeout(() => {
+            this.loading.dismiss();
+        }, 5000);
     }
 
     getProducto() {
         this.rest.getProduct(this.selectedItem.producto.id)
             .then(data => {
+                this.loading.dismiss();
                 this.producto = data;
             }).catch(err => {console.log(err)});
     }
--- comandas2app/src/pages/list/list.ts
+++ comandas2app/src/pages/list/list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, LoadingController} from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
 import {RestapiProvider} from "../../providers/restapi/restapi";
+
 
 @Component({
     selector: 'page-list',
@@ -13,11 +14,27 @@ export class ListPage {
 
     items: Array<{producto: any}>;
     categoria: any;
-
-    constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestapiProvider) {
-
+    loading: any;
+    
+    constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestapiProvider, public loadingCtrl: LoadingController) {
+        this.presentLoadingDefault();
         this.categoria = navParams.data.categoria;
         this.getProductsByCategorie();
+    }
+
+    public presentLoadingDefault() {
+        this.loading = this.loadingCtrl.create({
+            //spinner: 'dots',
+            //spinner: 'bubbles',
+            //spinner: 'circles',
+            spinner: 'crescent',
+        });
+
+        this.loading.present();
+
+        setTimeout(() => {
+            this.loading.dismiss();
+        }, 5000);
     }
 
     itemTapped(event, item) {
@@ -29,6 +46,7 @@ export class ListPage {
     getProductsByCategorie() {
         this.rest.getProductsByCategorie(this.navParams.data.categoria_id)
             .then((data: {items: any}) => {
+                this.loading.dismiss();
                 var items = [];
                 data.items.forEach(function(val, key){
                   items.push({producto: val});
